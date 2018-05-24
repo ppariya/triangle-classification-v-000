37a9a4b92783fe0963e55b52dfc516eb137510e4
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-05-24 05:58:04 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,12 @@
 class Triangle
   # write code here
-  attr_accessor :equilateral, :isosceles, :scalene
+  attr_accessor :equilateral, :isosceles, :scalene, :side_1, :side_2, :side_3
+  def initialize (1,2,3)
+    @side_1 = 1
+    @side_2 = 2
+    @side_3 = 3
+  end
+
   def kind(side_1,side_2,side_3)
     if side_1 == side_2 && side_2 == side_3
       self.equilateral
